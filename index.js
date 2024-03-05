8ba6ddfe0df91b2bc3e8862e88a09fa474746799
Dennis Somba <47259138+Dennis619@users.noreply.github.com>
2024-03-05 10:22:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("node:fs");
+
+/*
+fs.writeFile("message.txt", "Hello From Node JS!!", (err) =>{
+    if(err) throw err;
+    console.log("The file has been saved!");
+});
+*/
+
+//read file
+fs.readFile("message.txt", "utf-8", (err, data) =>{
+    if(err) throw err;
+    console.log( data);
+});
